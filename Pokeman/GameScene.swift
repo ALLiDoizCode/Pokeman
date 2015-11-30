3e8128a116bb9a96402d8229676b057f1ec01f3a
Jonathan Green <jonathangreen@Jonathans-iMac-2.local>
2015-11-30 16:17:55 -0500

    added 3 sprites,animation, and 1 sound

--- Pokeman/GameScene.swift
+++ Pokeman/GameScene.swift
@@ -7,28 +7,63 @@
 //
 
 import SpriteKit
+import AVFoundation
 
 class GameScene: SKScene {
     
     let myNumber:Int = 10
     let gameName:String = "Pokeman"
     var malePlayer:SKSpriteNode = SKSpriteNode()
-
     
+    let tapRect = UITapGestureRecognizer()
+
     override func didMoveToView(view: SKView) {
         
-         self.physicsBody = SKPhysicsBody(edgeLoopFromRect: self.frame)
+        self.physicsBody = SKPhysicsBody(edgeLoopFromRect: self.frame)
     
         if let theSpriteNode:SKSpriteNode = self.childNodeWithName("MaleSprite") as? SKSpriteNode {
             
             malePlayer = theSpriteNode
         }
         
+        tapRect.addTarget(self, action: "tappedView:")
+        tapRect.numberOfTapsRequired = 1
+        tapRect.numberOfTouchesRequired = 1
+        self.view!.addGestureRecognizer(tapRect)
+        
        
+    }
+    
+    
+    func tappedView(sender:UITapGestureRecognizer){
+        
+        var playSound:AVAudioPlayer = AVAudioPlayer()
+        
+        let SoundEffect:NSURL = NSBundle.mainBundle().URLForResource("Walking", withExtension: "mp3")!
+        do { playSound = try AVAudioPlayer(contentsOfURL: SoundEffect, fileTypeHint: nil) } catch _ { return print("file not found") }
+            playSound.numberOfLoops = 1
+            playSound.prepareToPlay()
+            playSound.play()
+        
+        var touchLocation:CGPoint = sender.locationInView(self.view!)
+        touchLocation = self.convertPointFromView(touchLocation)
+        
+        let move:SKAction = SKAction.moveTo(touchLocation, duration: 0.6)
+        move.timingMode = .EaseOut
+        
+       
+        
+        print("tapped")
+        
+        malePlayer.runAction(move) { () -> Void in
+            
+            playSound.stop()
+        }
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
        /* Called when a touch begins */
+        
         
     }
    
