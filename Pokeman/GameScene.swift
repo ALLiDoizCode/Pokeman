6371d1798378b5d8ee46f899300cf7907b345b67
Jonathan Green <jonathangreen@Jonathans-iMac-2.local>
2015-12-03 16:23:47 -0500

    added events and sound for events

--- Pokeman/GameScene.swift
+++ Pokeman/GameScene.swift
@@ -14,6 +14,9 @@ class GameScene: SKScene,SKPhysicsContactDelegate {
     let myNumber:Int = 10
     let gameName:String = "Pokeman"
     var malePlayer:SKSpriteNode = SKSpriteNode()
+    var scoreLabel:SKLabelNode = SKLabelNode()
+    var gameOverLabel:SKLabelNode = SKLabelNode()
+    var life = 5
     
     let tapRect = UITapGestureRecognizer()
 
@@ -26,6 +29,18 @@ class GameScene: SKScene,SKPhysicsContactDelegate {
         if let theSpriteNode:SKSpriteNode = self.childNodeWithName("MaleSprite") as? SKSpriteNode {
             
             malePlayer = theSpriteNode
+        }
+        
+        if let theLabelNode:SKLabelNode = self.childNodeWithName("score") as? SKLabelNode {
+            
+            scoreLabel = theLabelNode
+            scoreLabel.text = "\(life)"
+        }
+        
+        if let theLabelNode:SKLabelNode = self.childNodeWithName("GameOver") as? SKLabelNode {
+            
+            gameOverLabel = theLabelNode
+            gameOverLabel.hidden = true
         }
         
         tapRect.addTarget(self, action: "tappedView:")
@@ -82,8 +97,29 @@ class GameScene: SKScene,SKPhysicsContactDelegate {
             print("hit")
             
             let soundEffect:SKAction = SKAction.playSoundFileNamed("hit.mp3", waitForCompletion: false)
+            let gameOverSound:SKAction = SKAction.playSoundFileNamed("GameOver.mp3", waitForCompletion: false)
             
             malePlayer.runAction(soundEffect)
+            
+            life = life - 1
+            
+            scoreLabel.text = "\(life)"
+            
+            if life < 1 {
+                
+                scoreLabel.text = "You are Dead"
+                
+                self.view?.removeGestureRecognizer(tapRect)
+                
+                malePlayer.hidden = true
+                gameOverLabel.hidden = false
+                gameOverLabel.runAction((gameOverSound), completion: { () -> Void in
+                    
+                    self.gameOverLabel.removeAllActions()
+                
+                })
+                
+            }
             
             }else if contact.bodyA.categoryBitMask == 2 && contact.bodyB.categoryBitMask == 1  {
             
